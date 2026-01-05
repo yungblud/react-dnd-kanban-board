a3a7a841c44515e77fdc13ee2f84b9d0b7ca2b70
yungblud <killi8n@gmail.com>
2026-01-05 11:27:57 +0900

    chore: :art: css for kanban column

--- src/ui/kanban-column/kanban-column.tsx
+++ src/ui/kanban-column/kanban-column.tsx
@@ -1,7 +1,17 @@
+import styled from '@emotion/styled'
 import { memo } from 'react'
+
+const Column = styled.div`
+  border: 1px solid #ababab;
+  min-height: 520px;
+
+  & + & {
+    margin-left: 1.5rem;
+  }
+`
 
 export const KanbanColumn = memo(
   ({ state }: { state: 'todo' | 'in-progress' | 'done' }) => {
-    return <p>{state}</p>
+    return <Column>{state}</Column>
   }
 )
