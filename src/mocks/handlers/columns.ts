b1f560d4b79404f1749b39ad51f9ad153b635d9c
yungblud <killi8n@gmail.com>
2026-01-05 12:20:01 +0900

    feat: :zap: implement base api response common retrieve with error

--- src/mocks/handlers/columns.ts
+++ src/mocks/handlers/columns.ts
@@ -1,8 +1,9 @@
 import { http, HttpResponse } from 'msw'
 import { mockData } from '../data'
+import { retrieveResponse } from '../utils'
 
 export const columnHandlers = [
   http.get('/api/columns', () => {
-    return HttpResponse.json(mockData.initialColumns)
+    return HttpResponse.json(retrieveResponse(mockData.initialColumns))
   }),
 ]
