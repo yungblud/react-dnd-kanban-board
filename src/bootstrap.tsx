d3ee01e00abba44797a22a894d721cda808f8bba
yungblud <killi8n@gmail.com>
2026-01-05 17:43:05 +0900

    feat: implement OverlayProvider

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -4,13 +4,16 @@ import App from './App.tsx'
 import GlobalStyle from './styles/global-style.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { getQueryClient } from './lib/query-client.ts'
+import { OverlayProvider } from 'overlay-kit'
 
 const queryClient = getQueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <OverlayProvider>
+        <App />
+      </OverlayProvider>
     </QueryClientProvider>
     <GlobalStyle />
   </StrictMode>
