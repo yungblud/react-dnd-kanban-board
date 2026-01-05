ed0cafaca88ed0dc773936e51da0d05df6312d8b
yungblud <killi8n@gmail.com>
2026-01-06 00:38:19 +0900

    feat: implement move card mutation

---
+++ src/api/queries/use-move-card-mutation.ts
@@ -0,0 +1,16 @@
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query'
+import { api } from '../api'
+import type { ApiError } from '../api.error'
+
+type Options = UseMutationOptions<
+  Awaited<ReturnType<typeof api.moveCard>>,
+  ApiError,
+  Parameters<typeof api.moveCard>[0]
+>
+
+export function useMoveCardMutation(options?: Options) {
+  return useMutation({
+    mutationFn: (variables) => api.moveCard(variables),
+    ...options,
+  })
+}
