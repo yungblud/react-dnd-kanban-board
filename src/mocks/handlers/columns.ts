76c1aebc34f9829511046e0b3d269d821155d534
yungblud <killi8n@gmail.com>
2026-01-05 12:21:03 +0900

    feat: :art: implement http status json

--- src/mocks/handlers/columns.ts
+++ src/mocks/handlers/columns.ts
@@ -1,9 +1,12 @@
 import { http, HttpResponse } from 'msw'
 import { mockData } from '../data'
 import { retrieveResponse } from '../utils'
+import { HttpStatus } from '../types'
 
 export const columnHandlers = [
   http.get('/api/columns', () => {
-    return HttpResponse.json(retrieveResponse(mockData.initialColumns))
+    return HttpResponse.json(retrieveResponse(mockData.initialColumns), {
+      status: HttpStatus.SUCCESS,
+    })
   }),
 ]
