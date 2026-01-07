415e418cae6f4ba45805088b44b5f52b4f97e1e7
yungblud <killi8n@gmail.com>
2026-01-07 09:35:01 +0900

    refactor: memoize columns data

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,17 @@ function App() {
     refetchOnMount: false,
   })
 
-  const columnsData = useMemo(() => columns?.data ?? [], [columns?.data])
+  const columnsData = useMemo(() => {
+    if (!columns?.data) return []
+    return [...columns.data]
+      .sort((a, b) => a.order - b.order)
+      .map((column) => ({
+        ...column,
+        cards: [...column.cards]
+          .filter((card) => card.columnId === column.id)
+          .sort((a, b) => a.order - b.order),
+      }))
+  }, [columns])
 
   if (isLoadingColumns) {
     return null
@@ -24,25 +34,20 @@ function App() {
       addColumnBtn={<AddColumnButton />}
       kanban={
         <KanbanContainer>
-          {[...columnsData]
-            .sort((a, b) => a.order - b.order)
-            .map((column) => {
-              const cards = [...column.cards]
-                .filter((card) => card.columnId === column.id)
-                .sort((a, b) => a.order - b.order)
-              return (
-                <KanbanColumn
-                  key={column.id}
-                  {...column}
-                  // @TODO: enhance this prop
-                  cards={cards}
-                >
-                  {cards.map((card, index) => (
-                    <KanbanCard key={card.id} {...card} index={index} />
-                  ))}
-                </KanbanColumn>
-              )
-            })}
+          {columnsData.map((column) => {
+            return (
+              <KanbanColumn
+                key={column.id}
+                {...column}
+                // @TODO: enhance this prop
+                cards={column.cards}
+              >
+                {column.cards.map((card, index) => (
+                  <KanbanCard key={card.id} {...card} index={index} />
+                ))}
+              </KanbanColumn>
+            )
+          })}
         </KanbanContainer>
       }
     />
